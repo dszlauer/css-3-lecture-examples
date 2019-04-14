1f1f38bdd39a78ca81ab9eb4748830be41245154
Daniel Szlauer <dszlauer@Daniels-MacBook-Pro.local>
2019-04-14 10:52:47 -0700

    final changes

--- forclass/responive-mini/src/App.js
+++ forclass/responive-mini/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
+
+import Banner from "./components/Banner";
 import Header from "./components/Header";
 import "./App.css";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Header />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Banner />
+      </div>
+    );
+  }
 }
 
 export default App;
